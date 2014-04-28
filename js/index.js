09accadd68702860c2b4eb130b3f9202089ac175
Thomas Hobohm <superman3275@gmail.com>
2014-04-27 21:48:25 -0500

    Added in Find Me.

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(function() {
+	social = $(".social");
+	fadetime = 800;
+	// Start with it faded out
+	social.stop().fadeTo(0, 0.5);
+	social.mouseover(function() {
+		$(this).stop().fadeTo(fadetime, 1.0);
+	});
+	social.mouseout(function() {
+		$(this).stop().fadeTo(fadetime, 0.5);
+	});
+});
