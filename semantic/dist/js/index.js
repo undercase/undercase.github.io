1a4325e22825f7989d87553ce8c7fd3ef5247061
Thomas Hobohm <superman3275@gmail.com>
2016-08-15 00:52:40 -0500

    Made title fade out.

--- semantic/dist/js/index.js
+++ semantic/dist/js/index.js
@@ -1,13 +1,17 @@
 $(document).ready(function() {
   return $('.inner').hover(function() {
+    $(this).find('h2').stop().hide();
     return $(this).find('p').transition('stop all').transition({
       animation: 'scale',
       duration: '200ms'
     });
   }, function() {
     return $(this).find('p').transition('stop all').transition({
-      animate: 'fade out',
-      duration: '200ms'
+      animate: 'scale out',
+      duration: '100ms',
+      onComplete: function() {
+        return $(this).siblings('h2').stop().show();
+      }
     });
   });
 });
--- semantic/src/coffee/index.coffee
+++ semantic/src/coffee/index.coffee
@@ -1,11 +1,14 @@
 $(document).ready ->
   $('.inner').hover ->
+    $(this).find('h2').stop().hide()
     $(this).find('p').transition('stop all').transition({
       animation: 'scale',
       duration: '200ms'
     })
   , ->
     $(this).find('p').transition('stop all').transition({
-      animate: 'fade out',
-      duration: '200ms'
+      animate: 'scale out',
+      duration: '100ms',
+      onComplete: ->
+        $(this).siblings('h2').stop().show()
     })
