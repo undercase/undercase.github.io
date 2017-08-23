ebf321e522d2b65d7302fadd03e7b6377bb338a1
Thomas Hobohm <superman3275@gmail.com>
2017-08-22 23:14:41 -0500

    Changed animation to occur instantly.

--- js/index.js
+++ js/index.js
@@ -1,13 +1,11 @@
-$(document).ready(() => {
-  var img = document.getElementsByTagName('img')[0];
+var img = document.getElementsByTagName('img')[0];
 
-  var aquarelle = new Aquarelle(img, 'img/mask.png', {
-      autoplay: true
-  });
+var aquarelle = new Aquarelle(img, 'img/mask.png', {
+    autoplay: true
+});
 
-  aquarelle.addEventListener('created', function() {
-    var canvas = this.getCanvas();
-    img.parentNode.insertBefore(canvas, img.nextSibling);
-    img.parentNode.removeChild(img);
-  });
+aquarelle.addEventListener('created', function() {
+  var canvas = this.getCanvas();
+  img.parentNode.insertBefore(canvas, img.nextSibling);
+  img.parentNode.removeChild(img);
 });
