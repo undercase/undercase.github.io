4c2bae65861fffab3192c0bf24f3ff8bf6101f95
Thomas Hobohm <superman3275@gmail.com>
2017-08-22 22:55:01 -0500

    Added create event listener

--- js/index.js
+++ js/index.js
@@ -1,7 +1,14 @@
 $(document).ready(() => {
   var img = document.getElementsByTagName('img')[0];
+
   var aquarelle = new Aquarelle(img, 'img/mask.png', {
       autoplay: true,
       loop: true
+  });
+
+  aquarelle.addEventListener('created', function() {
+    var canvas = this.getCanvas();
+    img.parentNode.insertBefore(canvas, img.nextSibling);
+    img.parentNode.removeChild(img);
   });
 });
