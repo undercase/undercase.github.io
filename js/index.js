2456ec2874f585602ed3ff321c57e780819e5b7b
Thomas Hobohm <superman3275@gmail.com>
2017-08-29 13:09:10 -0500

    Added colors

--- js/index.js
+++ js/index.js
@@ -1,14 +1,41 @@
 $(document).ready(() => {
-  var img = document.getElementsByTagName('img')[0];
-
-  var aquarelle = new Aquarelle(img, 'img/mask.png', {
-      autoplay: true
+  $(".music").hover(() => {
+    $("body").css("background", "#DB3340");
+    $(".music").css("color", "#ffffff");
+    $("h1").css("color", "#ffffff");
+  }, () => {
+    $("body").css("background", "#ffffff");
+    $(".music").css("color", "#DB3340");
+    $("h1").css("color", "#000000");
   });
 
-  aquarelle.addEventListener('created', function() {
-    $('.container').css('display', 'block');
-    var canvas = this.getCanvas();
-    img.parentNode.insertBefore(canvas, img.nextSibling);
-    img.parentNode.removeChild(img);
+  $(".fashion").hover(() => {
+    $("body").css("background", "#E8B71A");
+    $(".fashion").css("color", "#fff");
+    $("h1").css("color", "#ffffff");
+  }, () => {
+    $("body").css("background", "#ffffff");
+    $(".fashion").css("color", "#E8B71A");
+    $("h1").css("color", "#000000");
+  });
+
+  $(".coding").hover(() => {
+    $("body").css("background", "#1FDA9A");
+    $(".coding").css("color", "#fff");
+    $("h1").css("color", "#ffffff");
+  }, () => {
+    $("body").css("background", "#ffffff");
+    $(".coding").css("color", "#1FDA9A");
+    $("h1").css("color", "#000000");
+  });
+
+  $(".philosophy").hover(() => {
+    $("body").css("background", "#28ABE3");
+    $(".philosophy").css("color", "#fff");
+    $("h1").css("color", "#ffffff");
+  }, () => {
+    $("body").css("background", "#ffffff");
+    $(".philosophy").css("color", "#28ABE3");
+    $("h1").css("color", "#000000");
   });
 });
