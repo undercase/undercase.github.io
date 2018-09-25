94aaacf63ae501b266b0e8ba2e237566864191bb
Thomas Hobohm <superman3275@gmail.com>
2018-09-25 16:45:25 -0500

    Remade homepage.

--- js/index.js
+++ js/index.js
@@ -3,39 +3,35 @@ $(document).ready(() => {
     $("body").css("background", "#DB3340");
     $(".music").css("color", "#ffffff");
     $("h1").css("color", "#ffffff");
+    $(".description").css("color", "#ffffff");
   }, () => {
     $("body").css("background", "#ffffff");
     $(".music").css("color", "#DB3340");
-    $("h1").css("color", "#000000");
+    $("h1").css("color", "#28ABE3");
+    $(".description").css("color", "#000000");
   });
 
   $(".fashion").hover(() => {
     $("body").css("background", "#E8B71A");
     $(".fashion").css("color", "#fff");
     $("h1").css("color", "#ffffff");
+    $(".description").css("color", "#ffffff");
   }, () => {
     $("body").css("background", "#ffffff");
     $(".fashion").css("color", "#E8B71A");
-    $("h1").css("color", "#000000");
+    $("h1").css("color", "#28ABE3");
+    $(".description").css("color", "#000000");
   });
 
   $(".coding").hover(() => {
     $("body").css("background", "#1FDA9A");
     $(".coding").css("color", "#fff");
     $("h1").css("color", "#ffffff");
+    $(".description").css("color", "#ffffff");
   }, () => {
     $("body").css("background", "#ffffff");
     $(".coding").css("color", "#1FDA9A");
-    $("h1").css("color", "#000000");
-  });
-
-  $(".philosophy").hover(() => {
-    $("body").css("background", "#28ABE3");
-    $(".philosophy").css("color", "#fff");
-    $("h1").css("color", "#ffffff");
-  }, () => {
-    $("body").css("background", "#ffffff");
-    $(".philosophy").css("color", "#28ABE3");
-    $("h1").css("color", "#000000");
+    $("h1").css("color", "#28ABE3");
+    $(".description").css("color", "#000000");
   });
 });
